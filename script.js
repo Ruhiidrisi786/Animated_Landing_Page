188ec9bdabd04f0a6755d0c802e875807c4f4d25
Rukaiya Bano <rukaiyabano888@gmail.com>
2024-04-26 09:38:46 -0700

    upload file

---
+++ script.js
@@ -0,0 +1,28 @@
+// gsap.to("#one", {
+//     width: "100%",
+//     ease: Expo.easeInOut,
+//     duration: 2,
+// })
+
+gsap.to(".imgBox", {
+    width: "100%",
+    ease: Expo.easeInOut,
+    duration: 2,
+    stagger: 2,
+    
+});
+
+
+gsap.to(".text h1",{
+    ease: Expo.easeInOut,
+    stagger: 2,
+    top: 0,
+});
+
+
+gsap.to(".text h1",{
+    delay: 2,
+    ease: Expo.easeInOut,
+    stagger: 2,
+    top: "-100%",
+});
